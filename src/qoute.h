308e48d28bfbdcaaf5a5b79974a4998f3aef828f
Calvinbullock <calvinnbullock@gmail.com>
2024-12-23 14:24:17 -0800

    moved src files into src

--- src/qoute.h
+++ src/qoute.h
@@ -7,19 +7,36 @@ using namespace std;
 class Qoute {
 
 public:
+    //
+    // constructors
     Qoute() : qoute(), author(), tags() {}
     Qoute(const string &qoute, const string &author)
          : qoute(qoute), author(author), tags() {}
 
+    //
+    // print / display
     void print() {
         std::cout << qoute << std::endl;
         std::cout << "- "<< author << std::endl;
+    }
+    void printTags() {
+        for (int i = 0; i < tags.size(); i++) {
+            std::cout << tags[i] << std::endl;
+        }
+    }
+
+    //
+    // getters / setters
+
+    //
+    // Other
+    void format() {
+        // TODO: add line brakes every x count of chars
     }
 
     void addQouteTag(const string &tag) {
         tags.push_back(tag);
     }
-
 
 protected:
     string qoute;
