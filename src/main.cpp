00d6064ab907c37a3d80c00c2d5c70b77e4e33af
Calvinbullock <calvinnbullock@gmail.com>
2024-12-27 18:34:42 -0800

    added test runner func to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 
 #include "cliArg.h"
 #include "quotes.h"
+#include <iostream>
+
+void runTest() {
+    std::cout << "Running Tests..." << std::endl;
+
+    std::cout << "\n" << std::endl;
+}
 
 int main (int argc, char *argv[]) {
+    runTest();
+
     CliArg cli = CliArg();
 
     Quotes quotes = Quotes();
