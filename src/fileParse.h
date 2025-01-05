a4c2bbc9c7aacabab95b968f82263729d1bf6ab5
Calvinbullock <calvinnbullock@gmail.com>
2025-01-04 17:22:38 -0800

    added some function comments to fileParse

--- src/fileParse.h
+++ src/fileParse.h
@@ -1,20 +1,42 @@
 #pragma once
 
+//
+// FILE PARSING FUNCTIONS
+// Parses quote files into quote objects
+//   and/or a quote into a file formate.
+
 #include "quote.h"
 #include <iostream>
 #include <string>
 
-/*
-   Parses quote files into quote objects.
-*/
-
+/* =========================================================
+ * find the lines key, AKA check that this line is representing
+ * Opts:
+ *    Quote
+ *    Author
+ *    tags []
+ */
 inline Quote parseFile(std::string fileName) {
    Quote q;
-
 
    return q;
 }
 
+/* =========================================================
+ * find the lines key, AKA check that this line is representing
+ * Opts:
+ *    Quote
+ *    Author
+ *    tags []
+ */
+inline std::string parseKey(std::string fileLine) {
+   return "";
+}
+
+/* =========================================================
+ * take a key and a fileLine then parse the line into an
+ *    Author / quote line and return that.
+ */
 inline std::string parseLine(std::string parseKey, std::string fileLine) {
    std::string quoteLine;
    std::string lineKey = fileLine.substr(0, parseKey.length());
